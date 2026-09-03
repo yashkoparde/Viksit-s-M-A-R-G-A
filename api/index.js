20ad555cd5b1aec6519823a6f40b354a2e094a05
yashkoparde <yashkoparde2022@gmail.com>
2026-09-03 10:02:00 +0530

    chore(docs): create vercel serverless express entrypoint - refinement 13/30

--- api/index.js
+++ api/index.js
@@ -6,3 +6,5 @@ module.exports = app;
 // Audit verification stamp: 2026-09-03 10:17:00 +0530
 
 // Audit verification stamp: 2026-09-03 10:37:00 +0530
+
+// Audit verification stamp: 2026-09-03 10:02:00 +0530
